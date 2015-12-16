c02589becbd36da2d0041f396a317717d40b6d13
sydneyjia <cyndileo@sina.com>
2015-12-16 15:28:12 +0800

    complete functions, exercise 2

--- exercise2.py
+++ exercise2.py
@@ -194,7 +194,81 @@ def valid_date_format(date_string):
     :param date_string: date to be checked
     :return: Boolean True if the format is valid, False otherwise
     """
+    # separate the date string by dashes
+    checklist = date_string.split('-')
 
+    # if total number of character groups is not three, the format is not valid
+    if len(checklist) != 3:
+        return False
+
+    # if the total number of digits in "year" is not 4 or they are not digits, the format is not valid
+    if len(checklist[0]) != 4 or not checklist[0].isdigit():
+        return False
+
+    # if the total number of digits in "month" is not 2 or they are not digits, the format is not valid
+    if len(checklist[1]) != 2 or not checklist[1].isdigit():
+        return False
+
+    # if the total number of digits in "day" is not 2 or they are not digits, the format is not valid
+    if len(checklist[2]) != 2 or not checklist[2].isdigit():
+        return False
+
+    # passed all checks, the format is valid
     return True
+
+def check_required_fields(single_entry):
+    
+    # check if there is one field missing
+    for item in REQUIRED_FIELDS:
+        if item not in single_entry:
+            return False
+    return True
+
+
+def known_location(location):
+
+    # check if the code is in the country list
+    code = location["country"].upper()
+    if code in COUNTRIES or code == "KAN":
+        return True
+    return False
+
+
+def check_quarantine(location):
+
+    # check if it needs medical advisory
+    code = location["country"].upper()
+    if code in COUNTRIES:
+        if COUNTRIES[code]['medical_advisory'] != '':
+            return True
+    return False
+
+
+def check_home(location):
+
+    # check if the home is KAN
+    code = location["country"].upper()
+    return code == "KAN"
+
+
+def check_visa_requirement(from_location, reason):
+
+    # check if it needs visa
+    code = from_location["country"].upper()
+    if code in COUNTRIES:
+        if COUNTRIES[code]['visitor_visa_required'] == '1' and reason == "visit":
+            return True
+    return False
+
+
+def conflict_decision_resolver(list_of_decisions):
+
+    # make decision if has conflict
+    if "Quarantine" in list_of_decisions:
+        return "Quarantine"
+    elif "Reject" in list_of_decisions:
+        return "Reject"
+    else:
+        return "Accept"
 
 
