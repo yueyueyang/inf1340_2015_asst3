6e58a4cc6536734f5a3e91f8f3cc5419d9190957
Yue <yueyue-0929@hotmail.com>
2015-12-16 23:34:47 -0500

    fixed test case

--- test_exercise2.py
+++ test_exercise2.py
@@ -5,7 +5,7 @@
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Susan S,im"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
@@ -28,56 +28,45 @@ def test_returning():
 
 def test_basic():
 
-    assert decide("test_returning_citizen.json", "watchlist.json", "countries.json") == ["Accept", "Accept"]
+    assert decide("test_watchlist.json", "countries.json") == ["Quarantine"]
 
-    assert decide("test_watchlist.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_quarantine.json", "countries.json") == ["Quarantine"]
 
-    assert decide("test_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_lowercase.json", "countries.json") == ["Accept"]
 
-    assert decide("test_lowercase.json", "watchlist.json", "countries.json") == ["Accept"]
+    assert decide("test_raises_all_flags.json", "countries.json") == ["Quarantine"]
 
-    assert decide("test_raises_all_flags.json", "watchlist.json", "countries.json") == ["Quarantine"]
-
-def test_files():
-
-    with pytest.raises(FileNotFoundError):
-
-        decide("test_returning_citizen.json", "", "countries.json")
-
-        decide("", "watchlist.json", "countries.json")
-
-        decide("test_returning_citizen.json", "watchlist.json", "")
 
 def test_empty():
 
-    assert decide("test_empty_fname.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_empty_fname.json", "countries.json") == ["Accept"]
 
-    assert decide("test_empty_lname.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_empty_lname.json", "countries.json") == ["Accept"]
 
-    assert decide("test_emptycountry.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_emptycountry.json", "countries.json") == ["Reject"]
 
-    assert decide("test_nopassport.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_nopassport.json", "countries.json") == ["Reject"]
 
 
 def test_wronginput():
 
-    assert decide("test_wrongpassport.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_wrongpassport.json", "countries.json") == ["Reject"]
 
-    assert decide("test_wrongformat.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_wrongformat.json", "countries.json") == ["Quarantine"]
 
-    assert decide("test_nonexistent_country.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_nonexistent_country.json", "countries.json") == ["Reject"]
 
 
 def test_wrongvisa():
 
-    assert decide("test_wrongvisa.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_wrongvisa.json", "countries.json") == ["Quarantine"]
 
 
 def test_compromised_citizen():
 
-    assert decide("test_returning_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_returning_quarantine.json", "countries.json") == ["Quarantine"]
 
-    assert decide("test_returning_watchlisted.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_returning_watchlisted.json", "countries.json") == ["Accept"]
 
 
 
